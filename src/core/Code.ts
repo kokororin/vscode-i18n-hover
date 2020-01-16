f297e1ea34349101e240a58db2ae1298b32404e6
kokororin <i@kotori.love>
2020-01-16 15:35:02 +0800

    Add cache

--- src/core/Code.ts
+++ src/core/Code.ts
@@ -3,13 +3,20 @@ import * as fs from 'fs';
 import * as babel from '@babel/core';
 
 export class Code {
+  private static cacheMap = new Map<string, string>();
+  private filePath: string;
   private code: string;
 
   public constructor(filePath: string) {
+    this.filePath = filePath;
     this.code = String(fs.readFileSync(filePath));
   }
 
   public transform() {
+    if (Code.cacheMap.has(this.filePath)) {
+      return Code.cacheMap.get(this.filePath)!;
+    }
+
     const result = babel.transform(this.code, {
       babelrc: false,
       presets: [
@@ -17,6 +24,7 @@ export class Code {
       ]
     });
     if (result && result.code) {
+      Code.cacheMap.set(this.filePath, result.code);
       return result.code;
     }
     return this.code;
